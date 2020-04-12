b3ffbc947abe2ae2c58e7807de525eb3c05df38d
Carlos Arantes <carlos@ipe.digital>
2020-04-12 17:44:40 -0300

    estudando node, fazendo um webscrap para automação de whatsApp

---
+++ index.js
@@ -0,0 +1,58 @@
+const express = require("express");
+const path = require('path');
+const puppeteer = require('puppeteer');
+var bodyParser = require('body-parser');
+
+let page = null;
+(async () => {
+    const browser = await puppeteer.launch({ headless: false });
+    return await browser.newPage();
+})().then( (p) => {
+    page = p;
+});
+
+var ex = express();
+ex.use(bodyParser.json()); // for parsing application/json
+ex.use(bodyParser.urlencoded({ extended: true })); 
+
+ex.get("/getQRCode",  async (req, res) => {
+    await page.goto("https://web.whatsapp.com/", { waitUntil: 'networkidle2' });
+
+    await page.waitForSelector('._11ozL');
+    const qrCode = page.evaluate( () => {
+        const divQrCode = document.getElementsByClassName("_11ozL")[0];
+        console.log(divQrCode);
+        return divQrCode;
+    });
+    
+    console.log(qrCode);
+    
+    res.send(qrCode);
+});
+
+ex.post("/sendMessage", async (req, res) => {
+    const phone = req.body.phone;
+    const message = req.body.message;
+
+    page.evaluate(() => {
+        let camposTexto = document.getElementsByClassName('_2S1VP copyable-text selectable-text');
+        if( typeof camposTexto != "undefined" && (camposTexto.length > 0) ){
+            if(camposTexto[1]){
+                camposTexto[1].textContent = '';
+            }
+        }
+    });
+
+    await page.goto("https://web.whatsapp.com/send?phone="+phone+"&text="+message, { waitUntil: 'networkidle2' });    
+    await page.waitForSelector('._35EW6');
+    const data = page.evaluate( () => {
+        const btnSend = document.getElementsByClassName("_35EW6")[0];
+        if(btnSend){
+            btnSend.click();
+        }
+    });
+    res.send({ status : true, message : "Mensagem enviada com sucesso!" });
+});
+
+ex.listen(3400);
+// await browser.close()
